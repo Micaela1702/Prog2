4dc1a8d585a53673412cc02de20a0453f7873589
Micaela Pina <kikapinagomez@gmail.com>
2025-11-06 11:16:22 -0300

    cambios

--- Practico12/src/Futbol5/Comparadores/ComparadorPorApellido.java
+++ Practico12/src/Futbol5/Comparadores/ComparadorPorApellido.java
@@ -5,9 +5,12 @@ import Futbol5.Socio;
 import java.util.Comparator;
 
 public class ComparadorPorApellido implements Comparator<Socio> {
-
     @Override
     public int compare(Socio socio1, Socio socio2) {
-        return socio1.getApellido().compareTo(socio2.getApellido());
+        int resultado = socio1.getApellido().compareTo(socio2.getApellido());
+        if(resultado == 0){
+            return socio1.getNombre().compareTo(socio2.getNombre());
+        }
+        return resultado;
     }
 }
